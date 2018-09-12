8d632b196615844c0fa356c6d344b8f8b84fde4e
Thomas Zwarts <thomaszwarts@gmail.com>
2018-09-12 19:53:37 +0200

    comments.. comments.. comments..

--- src/middleware/ExampleMiddleware.php
+++ src/middleware/ExampleMiddleware.php
@@ -11,6 +11,15 @@ use Qui\interfaces\IMiddleware;
  * Time: 19:06
  */
 
+/*
+ * This is an example middleware, middleware should always implement IMiddleware
+ * The next method gets called and depending on the outcome different things happen.
+ *
+ * if false:
+ *      stop request and respond with a 401 unauthorized
+ * if true:
+ *      just keep on truckin' on (adding the routes to the routes array to be served)
+ * */
 class ExampleMiddleware implements IMiddleware
 {
     public function next(): bool {
