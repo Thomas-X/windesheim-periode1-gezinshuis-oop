d45e679b01d0d82e89edbad62bf3857fdfd392bf
Mohamedhoesein <ricjoh_10@hotmail.com>
2018-09-27 14:40:58 +0200

    Added footer

--- views/layouts/app.php
+++ views/layouts/app.php
@@ -35,7 +35,36 @@
 <div class="mainContainer">
     <?php require($pagePath) ?>
 </div>
-<div class="rotate amazingexample">some footer perhaps</div>
-
+<!--<div class="rotate amazingexample">some footer perhaps</div> -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="mx-auto ml-md-auto col-md-4 fiftyWidth">
+                <h3>
+                    Contact gegevens
+                </h3>
+                <a>
+                    Email: lorem@ipsum.nl
+                </a>
+                <br>
+                <a>
+                    Telefoonnummer: 020-1234567
+                </a>
+            </div>
+            <div class="mx-auto mx-md-0 col-md-4 fiftyWidth">
+                <h3>
+                    Links
+                </h3>
+                <a href="/about">
+                    About us
+                </a>
+                <br>
+                <a href="/contact">
+                    Contact
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
