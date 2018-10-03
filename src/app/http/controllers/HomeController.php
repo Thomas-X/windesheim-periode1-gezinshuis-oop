7486338302656c9cb059a38e2c4deadaf34e7861
Thomas Zwarts <thomaszwarts@gmail.com>
2018-10-04 01:42:50 +0200

    misc

--- src/app/http/controllers/HomeController.php
+++ src/app/http/controllers/HomeController.php
@@ -35,7 +35,6 @@ class HomeController
      */
     public function showHome(Request $req, Response $res, $data)
     {
-        dd($data);
 //        $id = 1;
 //        Util::dd(Auth::login('Thomas', 'internetcat'));
 //        $users = DB::execute("SELECT * FROM user WHERE id=?", [$id]);
