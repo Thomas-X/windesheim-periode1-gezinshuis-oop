79a382e0a69c4f088bdf95e57dcf3fc14b996f1f
Ricardo <ricjoh_10@hotmail.com>
2018-10-04 19:53:30 +0200

    Change the view of the upload of the treatment so that on the upload button everywhere a hand is and not only at the edges. Added icons.

--- resources/views/pages/TreatmentDocument.php
+++ resources/views/pages/TreatmentDocument.php
@@ -7,8 +7,12 @@
 
         //Create the client selection
         echo '<div class="form-group">';
-        echo '<div class="input-group">';
         echo '<h4 class="title" for="client">Cliënt</h4>';
+        echo '<div class="input-group">';
+        echo '<div class="input-group-prepend">';
+        echo '<div class="input-group-text icon-form">';
+        echo '<i class="fas fa-user"></i>';
+        echo '</div>';
         echo '</div>';
         echo '<select class="form-control" id="client" name="client">';
         foreach ($clients as $id => $name)
@@ -17,17 +21,25 @@
         }
         echo '</select>';
         echo '</div>';
+        echo '</div>';
 
         //Create the file selection
         echo '<div class="form-group">';
-        echo '<div class="input-group">';
         echo '<h4 class="title" for="treatmentDocument">Behandel document</h4>';
+        echo '<div class="input-group">';
+        echo '<div class="input-group-prepend">';
+        echo '<div class="input-group-text icon-form icon-input">';
+        echo '<i class="fas fa-save"></i>';
         echo '</div>';
-        echo '<div class="btn btn-primary house-btn uploadFile">';
+        echo '</div>';
+        echo '<div class="fileUpload btn btn-primary house-btn">';
         echo '<span>Upload</span>';
         echo '<input type="file" id="treatmentDocument" name="treatmentDocument" class="upload" required>';
         echo '</div>';
+        echo '<div class="uploadFileParent">';
         echo '<a id="uploadFile">Geen bestand gekozen</a>';
+        echo '</div>';
+        echo '</div>';
         echo '</div>';
 
         echo '<button type="submit" class="btn btn-primary house-btn">Opslaan</button>';
