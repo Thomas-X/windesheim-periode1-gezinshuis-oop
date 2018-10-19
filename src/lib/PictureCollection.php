6448ec15c9e72d27110cde67085c471ae7eb4e84
Ricardo <ricjoh_10@hotmail.com>
2018-10-19 19:58:15 +0200

    Added pictures and collections table. Made the picture columns in profiles_employees and profiles_parents_caretakers and the pictures column in events a foreign key to the id in the collections table. Added a PictureCollection class with the signature of all the methods.

---
+++ src/lib/PictureCollection.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ricardo
+ * Date: 19-10-2018
+ * Time: 19:21
+ */
+
+namespace Qui\lib;
+
+
+class PictureCollection
+{
+    public static function createPictureCollection(array $pictures)
+    {
+    }
+
+    public static function deletePictureCollection(int $collectionId)
+    {
+    }
+
+    public static function deletePicture(int $collectionId, int $pictureId)
+    {
+    }
+
+    public static function updatePictureCollection(int $collectionId, int $pictureId, array $picture)
+    {
+    }
+
+    public static function getAllPicturesFromCollection(int $collectionId)
+    {
+    }
+
+    public static function getPictureFromCollection(int $collectionId, int $pictureId)
+    {
+    }
+}
