ac05a656f3466ba2e62f223c0fd46fd0212743ff
Ricardo <ricjoh_10@hotmail.com>
2018-10-20 16:28:06 +0200

    Changed the title of each form of the test page so that the method is also in the title.

--- resources/views/pages/Upload.php
+++ resources/views/pages/Upload.php
@@ -1,25 +1,25 @@
 <div class="container" style="min-height: 100vh;">
     <form action="<?= \Qui\lib\Routes::routes['uploadCollection'] ?>" method="post" enctype="multipart/form-data">
-        <?php \Qui\lib\Form::input('Upload collection', 'fa-folder-open',
+        <?php \Qui\lib\Form::input('createPictureCollection', 'fa-folder-open',
                     "<input type=\"file\" name=\"files[]\" multiple required>") ?>
         <button type="submit" class="btn btn-primary house-btn">Submit</button>
     </form>
     <hr>
     <form action="<?= \Qui\lib\Routes::routes['deleteCollection'] ?>" method="post">
-        <?php \Qui\lib\Form::input('Delete collection 1', 'fa-file-excel',
+        <?php \Qui\lib\Form::input('deletePictureCollection (Delete collection 1)', 'fa-file-excel',
             "<input type=\"hidden\" name=\"collectionId\" value=\"1\">") ?>
         <button type="submit" class="btn btn-primary house-btn">Submit</button>
     </form>
     <hr>
     <form action="<?= \Qui\lib\Routes::routes['deletePicture'] ?>" method="post">
-        <?php \Qui\lib\Form::input('Delete image 1 collection 1', 'fa-file-excel',
+        <?php \Qui\lib\Form::input('deletePicture (Delete image 1 collection 1)', 'fa-file-excel',
             "<input type=\"hidden\" name=\"collectionId\" value=\"1\">") ?>
             <input type="hidden" name="pictureId" value="1">
         <button type="submit" class="btn btn-primary house-btn">Submit</button>
     </form>
     <hr>
     <form action="<?= \Qui\lib\Routes::routes['updatePicture'] ?>" method="post" enctype="multipart/form-data">
-        <?php \Qui\lib\Form::input('Update image 1 collection 1', 'fa-arrow-up',
+        <?php \Qui\lib\Form::input('updatePictureCollection (Update image 1 collection 1)', 'fa-arrow-up',
             "<input type=\"file\" name=\"file\" required>") ?>
         <input type="hidden" name="collectionId" value="1">
         <input type="hidden" name="pictureId" value="1">
@@ -27,7 +27,7 @@
     </form>
     <hr>
     <form action="<?= \Qui\lib\Routes::routes['getAllPicturesFromCollection'] ?>" method="post">
-        <?php \Qui\lib\Form::input('Get images collection 1', 'fa-arrow-down',
+        <?php \Qui\lib\Form::input('getAllPicturesFromCollection (Get pictures collection 1)', 'fa-arrow-down',
             "<input type=\"hidden\" name=\"collectionId\" value=\"1\">") ?>
         <button type="submit" class="btn btn-primary house-btn">Submit</button>
     </form>
@@ -39,7 +39,7 @@
     ?>
     <hr>
     <form action="<?= \Qui\lib\Routes::routes['getPictureFromCollection'] ?>" method="post">
-        <?php \Qui\lib\Form::input('Get image 1 collection 1', 'fa-arrow-down',
+        <?php \Qui\lib\Form::input('getPictureFromCollection (Get picture 1 collection 1)', 'fa-arrow-down',
             "<input type=\"hidden\" name=\"collectionId\" value=\"1\">") ?>
         <input type="hidden" name="pictureId" value="1">
         <button type="submit" class="btn btn-primary house-btn">Submit</button>
