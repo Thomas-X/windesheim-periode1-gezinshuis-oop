fb07848c3933592a524ebff12e3b6197410370ad
Ricardo <ricjoh_10@hotmail.com>
2018-11-01 07:57:36 +0100

    Updated template to show a download button on the careforschema page. Added the controller for uploading/downloading the careforschema file.

--- resources/javascript/React-app/AllEntries.jsx
+++ resources/javascript/React-app/AllEntries.jsx
@@ -6,30 +6,36 @@ const AllEntries = () => {
 	const data = JSDATA.items;
 	const {baseUri, title, subtitle, newItemName, titleKey, secondTitleKey} = JSDATA;
 	const determineField = (val, idx) => {
-		return (
-				<li className="list-group-item" key={idx}>
-					<div className="row">
-						<div
-								className="col-sm-8 flexCenter">{titleKey && secondTitleKey ? (`${val[titleKey]} ${val[secondTitleKey]}`) : val[titleKey]}
-						</div>
-						<div className="col-sm-4 buttongrid">
-							<a href={`${baseUri}?id=${val.id}&type=update_get`}
-							   className="btn btn-success flex1" role="button">
-								<i className="fas fa-sync margin-2"></i>
-								&thinsp;Update
-							</a>
-							<form action={`${baseUri}?id=${val.id}&type=delete_post`} method="post">
-								<button type="submit"
-								        className="btn btn-danger" role="button"
-								        style={{width: '100%'}}>
-									<i className="fas fa-trash margin-2"></i>
-									&thinsp;Remove
-								</button>
-							</form>
-						</div>
-					</div>
-				</li>
-		);
+        return (
+            <li className="list-group-item" key={idx}>
+                <div className="row">
+                    <div
+                        className="col-sm-8 flexCenter">{titleKey && secondTitleKey ? (`${val[titleKey]} ${val[secondTitleKey]}`) : val[titleKey]}
+                    </div>
+                    <div className="col-sm-4 buttongrid">
+                        <a href={`${baseUri}?id=${val.id}&type=update_get`}
+                           className="btn btn-success flex1" role="button">
+                            <i className="fas fa-sync margin-2"></i>
+                            &thinsp;Update
+                        </a>
+                        <form action={`${baseUri}?id=${val.id}&type=delete_post`} method="post">
+                            <button type="submit"
+                                    className="btn btn-danger" role="button"
+                                    style={{width: '100%'}}>
+                                <i className="fas fa-trash margin-2"></i>
+                                &thinsp;Remove
+                            </button>
+                        </form>
+                        {location.pathname.indexOf("careforschemas") !== -1 ? (<button type="button"
+                                                                                       className="btn btn-info" role="button"
+                                                                                       style={{width: '100%'}}>
+                            <i className="fas fa-download margin-2"></i>
+                            &thinsp;Download
+                        </button>) : null}
+                    </div>
+                </div>
+            </li>
+        );
 	}
 	return (
 			<div className={'container'}>
---
+++ src/app/http/controllers/CareForSchemaController.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ricardo
+ * Date: 2-10-2018
+ * Time: 19:36
+ */
+
+namespace Qui\app\http\controllers;
+
+use Qui\lib\Request;
+use Qui\lib\Response;
+use Qui\lib\facades\View;
+use Qui\lib\facades\NotifierParser;
+use Qui\lib\Routes;
+
+/**
+ * Class CareForSchemaController
+ * @package Qui\app\http\controllers
+ */
+class CareForSchemaController
+{
+    private static $allowedFileExtensions = [
+                                                'pdf',
+                                                'doc',
+                                                'docx',
+                                                'odt',
+                                                'txt'
+                                            ];
+
+    private static $mimeTypes = [
+                                    'pdf' => 'application/pdf',
+                                    'doc' => 'application/msword',
+                                    'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                                    'odt' => 'application/vnd.oasis.opendocument.text',
+                                    'txt' => 'text/plain'
+                                ];
+
+
+    /**
+     * Show the clients the user is allowed to see.
+     * @param Request $req An object containing the information for the request
+     * @param Response $res An object for the response.
+     * @return mixed The view of all the clients will be returned.
+     */
+    public function showCareForSchemas(Request $req, Response $res)
+    {
+        //TODO: Get clients the user is allowed to see.
+        $clients= [
+            1 => 'kid 1',
+            2 => 'kid 2',
+            3 => 'kid 3',
+            4 => 'kid 4',
+            5 => 'kid 5',
+            6 => 'kid 6'
+        ];
+
+        if (isset($req->params['download_status']) && $req->params['download_status'] == 'success'){
+            NotifierParser::init()
+                ->newNotification()
+                ->success()
+                ->message('Behandel document is gedownload.');
+        }
+        return View::render('pages.CareForSchema', compact('clients'));
+    }
+
+    public function careForSchemasFile(Request $req, Response $res)
+    {
+        if (array_key_exists('upload', $req->params))
+            $this->uploadCareForSchemas($req, $res);
+        else
+            $this->downloadCareForSchemas($req, $res);
+    }
+
+    /**
+     * Upload the care for schema.
+     * @param Request $req An object containing the information for the request
+     * @param Response $res An object for the response.
+     * @return mixed The view of all the clients will be returned.
+     */
+    public function uploadCareForSchemas(Request $req, Response $res)
+    {
+
+        $uploadDir = 'uploads/';
+
+        //Get client id and uploaded file.
+        $clientId = trim($req->params['clientId']);
+        $file = $req->files['treatmentDocument'];
+
+        //Check if the parameters are valid.
+        if (isset($clientId) && $clientId !== '' && is_numeric($clientId) && isset($file))
+        {
+            $fileTmpName = $file['tmp_name'];
+
+            //Get file extension to check if it is valid.
+            $fileName = $file['name'];
+            $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+
+            if (in_array($fileExtension, self::$allowedFileExtensions))
+            {
+                //Check if file name exists.
+                $uploadPath = $uploadDir . $clientId . '.*';
+                $files = glob($uploadPath);
+
+                //Replace the star at the end of the upload path with the proper extension
+                $uploadPath = rtrim($uploadPath, '*') . $fileExtension;
+
+                if (is_array($files) && count($files) == 0)
+                {
+                    //If file does not exist on the server upload it.
+                    $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+                    if ($didUpload){
+                        //Notify the user the document was uploaded.
+                        NotifierParser::init()
+                            ->newNotification()
+                            ->success()
+                            ->message('Behandel document is geüpload.');
+                        return $this->showCareForSchemas($req, $res);
+                    }
+
+                    NotifierParser::init()
+                        ->newNotification()
+                        ->error()
+                        ->message('Er is iets fout gegaan tijdens het uploaden van het behandeldocument.');
+                    return $this->showCareForSchemas($req, $res);
+                }
+                else
+                {
+                    //If file does exist temporally rename it, upload the new file and remove the old file if th new file is uploaded.
+                    $tmpFileExtension = pathinfo($files[0], PATHINFO_EXTENSION);
+                    $tmpUploadPath = $uploadDir . $clientId . '.' . $tmpFileExtension;
+                    $tmpPath = $uploadDir . 'tmp.' . $fileExtension;
+                    $didChange = rename($tmpUploadPath, $tmpPath);
+
+                    if ($didChange)
+                    {
+                        $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+                        if ($didUpload)
+                        {
+                            $didDelete = unlink($tmpPath);
+                            if ($didDelete){
+                                NotifierParser::init()
+                                    ->newNotification()
+                                    ->success()
+                                    ->message('Behandel document is geüpload.');
+                                return $this->showCareForSchemas($req, $res);
+                            }
+                        }
+                    }
+
+                    NotifierParser::init()
+                        ->newNotification()
+                        ->error()
+                        ->message('Er is iets fout gegaan tijdens het overschrijven van het behandeldocument.');
+                    return $this->showCareForSchemas($req, $res);
+                }
+            }
+            //Notify the user not a valid document was given.
+            NotifierParser::init()
+                ->newNotification()
+                ->warning()
+                ->message('Fout type bestand. Het behandel document moet een .pdf, .doc, .docx, .odt of .txt bestand zijn.');
+            return $this->showCareForSchemas($req, $res);
+        }
+        elseif (!isset($clientId) || $clientId === '' || !is_numeric($clientId) ){
+            //Notify the user not a valid client was given.
+            NotifierParser::init()
+                ->newNotification()
+                ->warning()
+                ->message('Ongeldige cliënt geselecteerd.');
+            return $this->showCareForSchemas($req, $res);
+        }
+
+        //Notify the user no document was given.
+        NotifierParser::init()
+            ->newNotification()
+            ->warning()
+            ->message('Geen behandel document geupload.');
+        return $this->showCareForSchemas($req, $res);
+    }
+
+    /**
+     * Download the care for schema.
+     * @param Request $req An object containing the information for the request
+     * @param Response $res An object for the response.
+     * @return mixed The view of all the clients will be returned.
+     */
+    public function downloadCareForSchemas(Request $req, Response $res)
+    {
+        $clientId = trim($req->params['clientId']);
+
+        //Check if the parameters are valid.
+        if (isset($clientId) && $clientId !== "" && is_numeric($clientId)){
+            $files = glob( 'uploads\\' . $req->params['clientId'] . '.*');
+            if (count($files) > 0) {
+                $file = $files[0];
+                $extension = pathinfo($file, PATHINFO_EXTENSION);
+                $mimeType = self::$mimeTypes[$extension];
+                //TODO Show notification when download is finished.
+                header('Content-Description: File Transfer');
+                header('Content-Type: ' . $mimeType);
+                header('Content-Disposition: attachment; filename="' . $clientId . '.' . $extension . '"');
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize($file));
+                readfile($file);
+
+                return $res->redirect(Routes::routes['careForSchemas'] . '?download_status=success');
+            }
+            NotifierParser::init()
+                ->newNotification()
+                ->warning()
+                ->message('Er is geen behandel document voor de cliënt.');
+            return $this->showCareForSchemas($req, $res);
+        }
+        NotifierParser::init()
+            ->newNotification()
+            ->warning()
+            ->message('Ongeldige cliënt geselecteerd.');
+        return $this->showCareForSchemas($req, $res);
+    }
+}
