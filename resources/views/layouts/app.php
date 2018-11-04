a4bcf5444dd152ac5a38a305124e6087d5f351d7
Thomas Zwarts <thomaszwarts@gmail.com>
2018-11-04 02:27:58 +0100

    misc

--- resources/views/layouts/app.php
+++ resources/views/layouts/app.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <base href="http://localhost:8000"/>
+    <base href="https://adsd.clow.nl/~2018_p1_11/P1_OOAPP_Opdracht"/>
 
     <!-- Roboto font -->
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
